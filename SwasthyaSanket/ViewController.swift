b5a8c8f397c14089cfcc6ae17b71c85a7b8883bf
Mishu <divyanshmishu2004+code@gmail.com>
2025-07-12 13:29:36 +0530

    Initial Commit

---
+++ SwasthyaSanket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwasthyaSanket
+//
+//  Created by Divyansh Singhal on 12/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwasthyaSanketTests/SwasthyaSanketTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwasthyaSanketTests.swift
+//  SwasthyaSanketTests
+//
+//  Created by Divyansh Singhal on 12/07/25.
+//
+
+import Testing
+@testable import SwasthyaSanket
+
+struct SwasthyaSanketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
